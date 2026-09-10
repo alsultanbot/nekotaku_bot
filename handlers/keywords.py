04bd9ab622e31e38cc0595d75225654a85d94822
alsultanbot <8dv425n2dn@privaterelay.appleid.com>
2026-09-11 00:47:18 +0300

    add libraries

--- handlers/keywords.py
+++ handlers/keywords.py
@@ -68,7 +68,11 @@ async def keyword_handler(client: Client, message: Message) -> None:
         await message.reply_text(
             "📁 <b>الدرايف</b>\n\n"
             f"🔗 {LINKS['الدرايف']}"
-        )
+  elif "المكتبات" in text:
+    await message.reply_text(
+        "📚 <b>المكتبات</b>\n\n"
+        "🔗 https://m-itqan.com"
+    )
 
     elif "القروبات" in text or "قروبات" in text:
         groups_text = (
